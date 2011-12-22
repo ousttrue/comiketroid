7158a204cf2881141864d7ada98cf2200e98c0d8
ousttrue <ousttrue@gmail.com>
2011-12-22 17:04:36 +0900

    refactoring

--- src/main/java/jp/ousttrue/comiketroid/ComiketOpenHelper.java
+++ src/main/java/jp/ousttrue/comiketroid/ComiketOpenHelper.java
@@ -1,25 +1,17 @@
 package jp.ousttrue.comiketroid;
 
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
 import java.io.File;
 import android.content.Context;
-import au.com.bytecode.opencsv.CSVReader;
 import android.util.Log;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.os.Bundle;
 import android.app.AlertDialog;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.ContentValues;
-import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.Cursor;
-
 
 /**
  * http://www.comiket.co.jp/cd-rom/
@@ -55,23 +47,6 @@ class ComiketOpenHelper extends SQLiteOpenHelper {
     private static final String DROP_SQL=
         "DROP TABLE IF EXISTS "+TABLE
         ;
-
-    File dir;
-
-    void setDir(File dir)
-    {
-        this.dir=dir;
-    }
-
-    File getDir()
-    {
-        return dir;
-    }
-
-    File getCDATA()
-    {
-        return new File(dir, "CDATA");
-    }
 
     public ComiketOpenHelper(Context context, String name){
         super(context, String.format(DB, name), null, 1);
@@ -113,138 +88,15 @@ class ComiketOpenHelper extends SQLiteOpenHelper {
                 null, null, null, null, null, null);
     }
 
-    private ProgressDialog createProgressDialog(Activity context)
+    public void setup(Activity context, Handler onFinish)
     {
-      final ProgressDialog progressDialog = new ProgressDialog(context);
-      progressDialog.setTitle("初期化");
-      progressDialog.setMessage("データベース初期化中");
-      //progressDialog.setIndeterminate(true);
-      //progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-      progressDialog.setCancelable(false);
-      progressDialog.show();
-      return progressDialog;
-    }
-
-    public void setup(final Activity context, final Handler onFinish)
-    {
-        final SQLiteDatabase db=getWritableDatabase();
-        final ProgressDialog progressDialog=createProgressDialog(context);
-        final Handler handler = new Handler() {
-          @Override
-          public void handleMessage(Message msg) {
-            String progressMessaage=(String)msg.getData().get(
-                "progressMessaage").toString();
-            progressDialog.setMessage(progressMessaage);
-            Log.d(TAG, "handleMessage: "+progressMessaage);
-            Thread.yield();
-          }
-        };
-
         // /sdcard/Comiket/C81に決めうちにする
         File sdcard=Environment.getExternalStorageDirectory();
-        File dir=new File(sdcard, String.format("Comiket/"+name));
-        setDir(dir);
-
-        final ComiketOpenHelper helper=this;
 
         // 読み込み処理
-        final Thread t=new Thread(new Runnable(){
-
-            @Override
-            public void run(){
-                try{
-                    read();
-                }
-                finally{
-                    onFinish.sendEmptyMessage(0);
-                    progressDialog.dismiss();
-                }
-            }
-
-            // update dialog
-            private void sendMessage(String progressMessaage)
-            {
-                Bundle bundle = new Bundle();
-                bundle.putString("progressMessaage", progressMessaage);
-
-                Message message = new Message();
-                message.setData(bundle);
-
-                handler.sendMessage(message);
-                Thread.yield();
-            }
-
-            private void read(){
-                db.beginTransaction();
-                try{
-                    for(int i=1; i<=3; ++i){
-                        try{
-                            File f=new File(
-                                    getCDATA(), getDir().getName()+"ROM"+i+".TXT");
-                            Log.d(TAG, "open :"+f);
-                            CSVReader csv=new CSVReader(
-                                    new InputStreamReader(
-                                        new FileInputStream(f), "SJIS"), '\t');
-                            readCSV(csv);
-                        }
-                        catch(java.io.FileNotFoundException e){
-                            // skip directory
-                            e.printStackTrace();
-                        }
-                        catch(java.io.IOException e){
-                            e.printStackTrace();
-                            return;
-                        }
-                        // debug...
-                        //break;
-                    }
-                    db.setTransactionSuccessful();
-                    db.endTransaction();
-                }
-                finally{
-                    helper.close();
-                    Log.i(TAG, "closed");
-                }
-            }
-
-            private void readCSV(CSVReader csv)
-                throws java.io.IOException
-            {
-                String[] line;
-                ContentValues cv = new ContentValues();
-                String lastBlock="";
-                while ((line = csv.readNext()) != null) {
-                    if(!lastBlock.equals(line[6])){
-                      sendMessage("("+line[4]+")"+line[5]+line[6]);
-                      if(!"".equals(lastBlock)){
-                        // debug...
-                        //break;
-                      }
-                      lastBlock=line[6];
-                    }
-                    cv.clear();
-                    cv.put("x", line[0]);
-                    cv.put("y", line[1]);
-                    cv.put("page", line[2]);
-                    cv.put("cut", line[3]);
-                    cv.put("weekday", line[4]);
-                    cv.put("area", line[5]);
-                    cv.put("block", line[6]);
-                    cv.put("space", line[7]);
-                    cv.put("genre", line[8]);
-                    cv.put("name", line[9]);
-                    cv.put("kana", line[10]);
-                    cv.put("author", line[11]);
-                    cv.put("publish", line[12]);
-                    cv.put("url", line[13]);
-                    cv.put("email", line[14]);
-                    cv.put("comment", line[15]);
-                    db.insert(TABLE, "", cv);
-                }
-            }
-        });
+        final Thread t=new Thread(new Logic(context, this, onFinish,
+              new File(sdcard, String.format("Comiket/"+name))));
         t.start();
     }
-
 }
 
